0f802442f29d0db737e92a03082512bebacb1de8
DaniAffCH <danieleaffinita2000@gmail.com>
2024-02-06 19:13:20 +0100

    add goalie

--- website/index.js
+++ website/index.js
@@ -1,4 +1,4 @@
-const csvHeader = "ip_sender,roboCup_familiarity, soccer_familiarity, age, gender ,r1t1_x,r1t1_y,r2t1_x,r2t1_y,r3t1_x,r3t1_y,r4t1_x,r4t1_y,r5t1_x,r5t1_y,r6t1_x,r6t1_y,r1t2_x,r1t2_y,r2t2_x,r2t2_y,r3t2_x,r3t2_y,r4t2_x,r4t2_y,r5t2_x,r5t2_y,r6t2_x,r6t2_y,ball_holder,pass_to"
+const csvHeader = "ip_sender,roboCup_familiarity, soccer_familiarity, age, gender ,r1t1_x,r1t1_y,r2t1_x,r2t1_y,r3t1_x,r3t1_y,r4t1_x,r4t1_y,r5t1_x,r5t1_y,r6t1_x,r6t1_y,r7t1_x,r7t1_y,r1t2_x,r1t2_y,r2t2_x,r2t2_y,r3t2_x,r3t2_y,r4t2_x,r4t2_y,r5t2_x,r5t2_y,r6t2_x,r6t2_y,r7t2_x,r7t2_y,ball_holder,pass_to"
 const fileName = "data_v0.csv"
 
 const express = require('express');
